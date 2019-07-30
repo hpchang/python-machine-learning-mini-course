e8d3d9b38e886681eaf7efea8144f593f12587c9
HP Chang <hp.chang@hpchang.com>
2019-07-31 06:41:42 +0800

    lession 06: print standardize and normalize data

--- lession 06/sample.py
+++ lession 06/sample.py
@@ -10,15 +10,21 @@ array = dataframe.values
 # separate array into input and output components
 X = array[:,0:8]
 Y = array[:,8]
+
+print(numpy.shape(X),numpy.shape(Y))
+np = numpy.array(X)
+print(np[0:4,:-2])
+
 scaler = StandardScaler().fit(X)
 rescaledX = scaler.transform(X)
-
 # print Y
-for i in range(len(X)):
-    for j in range(len(X[i])):
-        print(X[i][j], end=' ')
-    print()
+#for i in range(len(X)):
+#    for j in range(len(X[i])):
+#        print(X[i][j], end=' ')
+#    print()
 
 # summarize transformed data
 numpy.set_printoptions(precision=3)
 print(rescaledX[0:5,:])
+rescaledXframe = pandas.DataFrame(rescaledX)
+print(rescaledXframe.describe())
